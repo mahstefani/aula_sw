fba6142882165a70a48da9855f39cfa674afdfa9
Daniel <42379129+danielirano@users.noreply.github.com>
2022-10-20 10:45:44 -0300

    Update dadosalunos.php

--- dadosalunos.php
+++ dadosalunos.php
@@ -9,6 +9,8 @@
         echo "Seguem abaixo as informações digitadas na pániga anterior: "."<br><br>";
         echo "Nome Completo: ".$_POST['txtnome']." ".$_POST['txtsobrenome']."<br>";
         echo "Telefone: ".$_POST['txttel']."<br>";
+        echo "Curso: ".$_POST['txtcurso']."<br>";
+        echo '<p style="color:black">'."Módulo: ".$_POST['txtmod']."<br>".'</p>';
     
     ?>
 </body>
